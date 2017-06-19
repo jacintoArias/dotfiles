9d8bee1a387182ddf6f7510d41a2f6ecc6881ce4
Jacinto Arias <arias.jacinto@gmail.com>
2017-06-19 14:27:44 +0200

    Improved tmux config

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -1,67 +1,30 @@
-#### COLOUR
 
-onedark_black="#282c34"
-onedark_blue="#61afef"
-onedark_yellow="#e5c07b"
-onedark_red="#e06c75"
-onedark_white="#aab2bf"
-onedark_green="#98c379"
-onedark_visual_grey="#3e4452"
-onedark_comment_grey="#5c6370"
 
-tm_icon="♟"
-tm_color_active=$onedark_green
-tm_color_inactive=$onedark_white
-tm_color_feature=$onedark_green
-tm_active_border_color=$onedark_white
+theme_grey="#373b41"
 
-# separators
-tm_separator_left_bold="◀"
-tm_separator_left_thin="❮"
-tm_separator_right_bold="▶"
-tm_separator_right_thin="❯"
+#"tm_icon=""
+tm_icon=""
+#tm_icon=""
 
-set -g status-left-length 32
+#tm_separator_right=""
+tm_separator_right=""
+
+set -g status-left-length 40
 set -g status-right-length 150
-set -g status-interval 5
+set -g status-interval 60
 
+# Highlight active window.
+set -w -g window-status-current-bg red
+set -g pane-active-border-fg 8
 
-# default statusbar colors
-# set-option -g status-bg colour0
-set-option -g status-fg $tm_color_active
-set-option -g status-bg default
-set-option -g status-attr default
+# Status bar.
+set -g status-style 'bg=#373b41,fg=white,bold,italics'
 
-# default window title colors
-set-window-option -g window-status-fg $tm_color_inactive
-set-window-option -g window-status-bg default
-set -g window-status-format "#I #W"
+tm_host="#[fg=yellow,bold]${USER}@#h"
+tm_date="#[fg=magenta,bold] %d %b %R  "
+tm_session_name="  #[fg=yellow,noitalics]$tm_icon #S #[fg=yellow]$tm_separator_right  "
 
-# active window title colors
-set-window-option -g window-status-current-fg $tm_color_active
-set-window-option -g window-status-current-bg default
-set-window-option -g  window-status-current-format "#[bold]#I #W"
-
-# pane border
-set-option -g pane-border-fg $tm_color_inactive
-set-option -g pane-active-border-fg $tm_active_border_color
-
-# message text
-set-option -g message-bg default
-set-option -g message-fg $tm_color_active
-
-# pane number display
-set-option -g display-panes-active-colour $tm_color_active
-set-option -g display-panes-colour $tm_color_inactive
-
-# clock
-set-window-option -g clock-mode-colour $tm_color_active
-
-#jtm_battery="#(~/.dotfiles/bin/battery_indicator.sh)"
-
-tm_date="#[fg=$tm_color_inactive] %R %d %b"
-tm_host="#[fg=$tm_color_feature,bold]${USER}@#h"
-tm_session_name="#[fg=$tm_color_feature,bold]$tm_icon #S #[fg=$tm_color_feature,nobold]$tm_separator_right_bold "
+tm_battery="#(~/.dotfiles/bin/battery_indicator.sh)  "
 
 set -g status-left $tm_session_name 
-set -g status-right $tm_date' '$tm_host
+set -g status-right $tm_host' '$tm_date' '$tm_battery
